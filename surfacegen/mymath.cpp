c1ce750091e45d13998e921e83f83482662e4bae
Иван Осоргин <igosorgin@edu.hse.ru>
2019-12-18 18:58:15 +0300

    i made classes and splited code into different files

---
+++ surfacegen/mymath.cpp
@@ -0,0 +1,6 @@
+#include "mymath.h"
+
+float mymath::dist(int x1, int y1, int x2, int y2)
+{
+	return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
---
+++ surfacegen/mymath.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <algorithm>
+
+static class mymath
+{
+public:
+	static float dist(int x1, int y1, int x2, int y2);
+};
+
